c7a32f9db7ee7ab5913d480661f6424d1c7ba6af
Elias <3lias.pappas@gmail.com>
2017-01-22 20:21:36 +0100

    Finished HW0 2.2

--- HW0/ML_HW0_2_2.m
+++ HW0/ML_HW0_2_2.m
@@ -1,5 +1,44 @@
+%load dataset into
 X = load('dataset0.txt');
 
-Covariance = cov(X);
-Correlation = corrcoef(X);
+%normalize X for each column between [0,1]
+Y = X - min(X);
+Y = Y ./ max(Y);
+
+%covariance of X and Y
+covX = cov(X);
+covY = cov(Y);
+%correlation of x and Y
+corrX = corrcoef(X);
+corrY = corrcoef(Y);
+
+%plot the colormaps
+figure;
+colormap default;
+
+subplot(2,2,1)
+imagesc(covX);
+title('Covariance X')
+colorbar
+
+subplot(2,2,2)
+imagesc(covY);
+title('Covariance Y')
+colorbar
+
+subplot(2,2,3)
+imagesc(corrX);
+title('Correlation  X')
+colorbar
+
+subplot(2,2,4)
+imagesc(corrY);
+title('Correlation  Y')
+colorbar
+
+figure;
+minCorrY = min(corrY(:));
+[minCorrRow,minCorrCol] = find(corrY == minCorrY);
+scatter(Y(:,minCorrRow(1)),Y(:,minCorrCol(1)));
+title(sprintf('Feature indices %i and %i with correlation value %0.3f',minCorrRow(1),minCorrCol(1),minCorrY));
 
