23d4c609415a5d910349d6f92fd3ad15371daac6
Joshua Jungen <joshua.jungen@outlook.de>
2017-03-06 15:04:53 +0100

    Solved 1.2 c)

--- HW5/problem2/problem1_2c.m
+++ HW5/problem2/problem1_2c.m
@@ -1,1 +1,12 @@
-%% TODO
+load('medium_100_10k');
+
+N = 1000;   % sample size
+k = 10;     % k-means
+
+samples = datasample(wordembeddings, N, 'Replace', false);
+IDX = kmeans(samples, k);
+samples2D = tsne(samples);
+
+fig = figure();
+gscatter(samples2D(:, 1), samples2D(:, 2), IDX, '', '.ox+*sdv^p');
+saveas(fig, 'problem2c.jpg')
